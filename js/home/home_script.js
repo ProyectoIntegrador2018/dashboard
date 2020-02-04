c955b4da988eee2c839a0967a5af5720844b692b
kawer <kawer7@gmail.com>
2020-02-04 16:01:25 -0600

    Linked front and backend

--- js/home/home_script.js
+++ js/home/home_script.js
@@ -1,6 +1,6 @@
 
 var myJson;
-var s = $("#card-container");
+var cardContainer = $("#card-container");
 var sidebar = $("#slide-out");
 
 
@@ -58,32 +58,35 @@ function getData(){
                     */
                 
                     myJson = JSON.parse('{"data":	[{ "type": "electrico" , "events":[ {"date": "2019-01-01", "duration": 10.2, "limits": { "min": 2, "max": 10 }, "anormal": true}, {"date": "2019-01-02", "duration": 8.1, "limits": { "min": 2, "max": 10 }, "anormal": false},{"date": "2019-01-03", "duration": 11.2, "limits": { "min": 2, "max": 10 }, "anormal": true}], "nevents":3, "anevents":2 }, { 		"type": "mecanico" , "events":[ {"date": "2019-01-01", "duration": 10.2, "limits": { "min": 2, "max": 10 }, "anormal": true}, {"date": "2019-01-02", "duration": 8.1, "limits": { "min": 2, "max": 10 }, "anormal": false},{"date": "2019-01-03", "duration": 11.2, "limits": { "min": 2, "max": 10 }, "anormal": true}], "nevents":3, "anevents":5},{ 		"type": "a" , "events":[ {"date": "2019-01-01", "duration": 10.2, "limits": { "min": 2, "max": 10 }, "anormal": true}, {"date": "2019-01-02", "duration": 8.1, "limits": { "min": 2, "max": 10 }, "anormal": false},{"date": "2019-01-03", "duration": 11.2, "limits": { "min": 2, "max": 10 }, "anormal": true}], "nevents":3, "anevents":1},	{		"type": "b" , "events":[ {"date": "2019-01-01", "duration": 10.2, "limits": { "min": 2, "max": 10 }, "anormal": true}, {"date": "2019-01-02", "duration": 8.1, "limits": { "min": 2, "max": 10 }, "anormal": false},{"date": "2019-01-03", "duration": 11.2, "limits": { "min": 2, "max": 10 }, "anormal": true}], "nevents":3, "anevents":2 }, { 		"type": "c" , "events":[ {"date": "2019-01-01", "duration": 10.2, "limits": { "min": 2, "max": 10 }, "anormal": true}, {"date": "2019-01-02", "duration": 8.1, "limits": { "min": 2, "max": 10 }, "anormal": false},{"date": "2019-01-03", "duration": 11.2, "limits": { "min": 2, "max": 10 }, "anormal": true}], "nevents":3, "anevents":5}, { 		"type": "d" , "events":[ {"date": "2019-01-01", "duration": 10.2, "limits": { "min": 2, "max": 10 }, "anormal": true}, {"date": "2019-01-02", "duration": 8.1, "limits": { "min": 2, "max": 10 }, "anormal": false},{"date": "2019-01-03", "duration": 11.2, "limits": { "min": 2, "max": 10 }, "anormal": true}], "nevents":3, "anevents":1 }, { 		"type": "e" , "events":[ {"date": "2019-01-01", "duration": 10.2, "limits": { "min": 2, "max": 10 }, "anormal": true}, {"date": "2019-01-02", "duration": 8.1, "limits": { "min": 2, "max": 10 }, "anormal": false},{"date": "2019-01-03", "duration": 11.2, "limits": { "min": 2, "max": 10 }, "anormal": true}], "nevents":3, "anevents":2 }, { 		"type": "f" , "events":[ {"date": "2019-01-01", "duration": 10.2, "limits": { "min": 2, "max": 10 }, "anormal": true}, {"date": "2019-01-02", "duration": 8.1, "limits": { "min": 2, "max": 10 }, "anormal": false},{"date": "2019-01-03", "duration": 11.2, "limits": { "min": 2, "max": 10 }, "anormal": true}], "nevents":3, "anevents":5},{ 		"type": "g" , "events":[ {"date": "2019-01-01", "duration": 10.2, "limits": { "min": 2, "max": 10 }, "anormal": true}, {"date": "2019-01-02", "duration": 8.1, "limits": { "min": 2, "max": 10 }, "anormal": false},{"date": "2019-01-03", "duration": 11.2, "limits": { "min": 2, "max": 10 }, "anormal": true}], "nevents":3, "anevents":1},	{		"type": "h" , "events":[ {"date": "2019-01-01", "duration": 10.2, "limits": { "min": 2, "max": 10 }, "anormal": true}, {"date": "2019-01-02", "duration": 8.1, "limits": { "min": 2, "max": 10 }, "anormal": false},{"date": "2019-01-03", "duration": 11.2, "limits": { "min": 2, "max": 10 }, "anormal": true}], "nevents":3, "anevents":2 }, { 		"type": "i" , "events":[ {"date": "2019-01-01", "duration": 10.2, "limits": { "min": 2, "max": 10 }, "anormal": true}, {"date": "2019-01-02", "duration": 8.1, "limits": { "min": 2, "max": 10 }, "anormal": false},{"date": "2019-01-03", "duration": 11.2, "limits": { "min": 2, "max": 10 }, "anormal": true}], "nevents":3, "anevents":5}, { 		"type": "j" , "events":[ {"date": "2019-01-01", "duration": 10.2, "limits": { "min": 2, "max": 10 }, "anormal": true}, {"date": "2019-01-02", "duration": 8.1, "limits": { "min": 2, "max": 10 }, "anormal": false},{"date": "2019-01-03", "duration": 11.2, "limits": { "min": 2, "max": 10 }, "anormal": true}], "nevents":3, "anevents":1 },{		 "type": "k" , "events":[ {"date": "2019-01-01", "duration": 10.2, "limits": { "min": 2, "max": 10 }, "anormal": true}, {"date": "2019-01-02", "duration": 8.1, "limits": { "min": 2, "max": 10 }, "anormal": false},{"date": "2019-01-03", "duration": 11.2, "limits": { "min": 2, "max": 10 }, "anormal": true}], "nevents":3, "anevents":2 }, { 		"type": "l" , "events":[ {"date": "2019-01-01", "duration": 10.2, "limits": { "min": 2, "max": 10 }, "anormal": true}, {"date": "2019-01-02", "duration": 8.1, "limits": { "min": 2, "max": 10 }, "anormal": false},{"date": "2019-01-03", "duration": 11.2, "limits": { "min": 2, "max": 10 }, "anormal": true}], "nevents":3, "anevents":5},{ 		"type": "m" , "events":[ {"date": "2019-01-01", "duration": 10.2, "limits": { "min": 2, "max": 10 }, "anormal": true}, {"date": "2019-01-02", "duration": 8.1, "limits": { "min": 2, "max": 10 }, "anormal": false},{"date": "2019-01-03", "duration": 11.2, "limits": { "min": 2, "max": 10 }, "anormal": true}], "nevents":3, "anevents":1},	{		"type": "n" , "events":[ {"date": "2019-01-01", "duration": 10.2, "limits": { "min": 2, "max": 10 }, "anormal": true}, {"date": "2019-01-02", "duration": 8.1, "limits": { "min": 2, "max": 10 }, "anormal": false},{"date": "2019-01-03", "duration": 11.2, "limits": { "min": 2, "max": 10 }, "anormal": true}], "nevents":3, "anevents":2 }, { 		"type": "o" , "events":[ {"date": "2019-01-01", "duration": 10.2, "limits": { "min": 2, "max": 10 }, "anormal": true}, {"date": "2019-01-02", "duration": 8.1, "limits": { "min": 2, "max": 10 }, "anormal": false},{"date": "2019-01-03", "duration": 11.2, "limits": { "min": 2, "max": 10 }, "anormal": true}], "nevents":3, "anevents":5}, { 		"type": "p" , "events":[ {"date": "2019-01-01", "duration": 10.2, "limits": { "min": 2, "max": 10 }, "anormal": true}, {"date": "2019-01-02", "duration": 8.1, "limits": { "min": 2, "max": 10 }, "anormal": false},{"date": "2019-01-03", "duration": 11.2, "limits": { "min": 2, "max": 10 }, "anormal": true}], "nevents":3, "anevents":1 }, { 		"type": "q" , "events":[ {"date": "2019-01-01", "duration": 10.2, "limits": { "min": 2, "max": 10 }, "anormal": true}, {"date": "2019-01-02", "duration": 8.1, "limits": { "min": 2, "max": 10 }, "anormal": false},{"date": "2019-01-03", "duration": 11.2, "limits": { "min": 2, "max": 10 }, "anormal": true}], "nevents":3, "anevents":2 }, { 		"type": "r" , "events":[ {"date": "2019-01-01", "duration": 10.2, "limits": { "min": 2, "max": 10 }, "anormal": true}, {"date": "2019-01-02", "duration": 8.1, "limits": { "min": 2, "max": 10 }, "anormal": false},{"date": "2019-01-03", "duration": 11.2, "limits": { "min": 2, "max": 10 }, "anormal": true}], "nevents":3, "anevents":5},{ 		"type": "s" , "events":[ {"date": "2019-01-01", "duration": 10.2, "limits": { "min": 2, "max": 10 }, "anormal": true}, {"date": "2019-01-02", "duration": 8.1, "limits": { "min": 2, "max": 10 }, "anormal": false},{"date": "2019-01-03", "duration": 11.2, "limits": { "min": 2, "max": 10 }, "anormal": true}], "nevents":3, "anevents":1},	{		"type": "t" , "events":[ {"date": "2019-01-01", "duration": 10.2, "limits": { "min": 2, "max": 10 }, "anormal": true}, {"date": "2019-01-02", "duration": 8.1, "limits": { "min": 2, "max": 10 }, "anormal": false},{"date": "2019-01-03", "duration": 11.2, "limits": { "min": 2, "max": 10 }, "anormal": true}], "nevents":3, "anevents":2 }, { 		"type": "u" , "events":[ {"date": "2019-01-01", "duration": 10.2, "limits": { "min": 2, "max": 10 }, "anormal": true}, {"date": "2019-01-02", "duration": 8.1, "limits": { "min": 2, "max": 10 }, "anormal": false},{"date": "2019-01-03", "duration": 11.2, "limits": { "min": 2, "max": 10 }, "anormal": true}], "nevents":3, "anevents":5}, { 		"type": "v" , "events":[ {"date": "2019-01-01", "duration": 10.2, "limits": { "min": 2, "max": 10 }, "anormal": true}, {"date": "2019-01-02", "duration": 8.1, "limits": { "min": 2, "max": 10 }, "anormal": false},{"date": "2019-01-03", "duration": 11.2, "limits": { "min": 2, "max": 10 }, "anormal": true}], "nevents":3, "anevents":1 }]}');
-                
+                	
+                	myJson = dataReceived;
                 
                 
 					//Aqui se ordena el json de fallas de mayor numero de eventos anomalos a menor
 					orderFallas();
 
-					var amountButtons = Math.floor(myJson.data.length / 10);
+					var amountButtons = Math.floor(myJson.length / 10);
 
-					if ((myJson.data.length%10) > 0)
+					if ((myJson.length%10) > 0)
 						amountButtons = amountButtons + 1;
 
 					console.log("Botones: " + amountButtons);
 
 					//Aqui se popula el dashboard con la informacion de la API
 					var newElement='';
-					for (i = 0; i < myJson.data.length; i++) { 
-			    		//cambié de span a div 
-			    		newElement += '<div class="row"><div class="col s10 m3"><div class="card"><div class="card-image"><img src="../images/placeholder.png"><span class="card-title" align="center">Falla ' + myJson.data[i].type + '</span></div><div class="card-content"><p>Cantidad de eventos: ' + myJson.data[i].events.length + "\n Eventos anómalos: " + myJson.data[i].anevents + " \n Orden: " + (i+1) + '</p></div><div class="card-action"><a id="'+ myJson.data[i].type +'" href="/DetallesFalla">Inspeccionar</a></div></div></div>';
+					for (i = 0; i < myJson.length; i++) { 
+			    		//cambié de span a div    	
+			 			newElement += '<div class="row"><div class="col s10 m3"><div class="card"><p class="card-title" align="center" >Falla ' + myJson[i].type + '</p><div class="card-content" style="padding-top:0px;">'+ '<p class="emphasize" "align="center" >' + myJson[i].anevents + '</p><p align="center"> Eventos anómalos</p><p align="center">Cantidad de eventos: ' + myJson[i].events.length  +  '</p></div><div class="card-action"><a id="'+ myJson[i].type +'" align="center">Inspeccionar</a></div></div></div>';
+			    		//newElement += '<div class="row"><div class="col s10 m3"><div class="card"><div class="card-image"><img src="../images/placeholder.png"><span class="card-title" align="center">Falla ' + myJson.data[i].type + '</span></div><div class="card-content"><p>Cantidad de eventos: ' + myJson.data[i].events.length + "\n Eventos anómalos: " + myJson.data[i].anevents + " \n Orden: " + (i+1) + '</p></div><div class="card-action"><a id="'+ myJson.data[i].type +'" href="/DetallesFalla">Inspeccionar</a></div></div></div>';
+
 			    	}
 
 			    	cardContainer.append(newElement);
 
 
 			    	//Ponle un listener a todos los boton 'inspect' de cada card
-			    	for (i = 0; i < myJson.data.length; i++) { 
-			    		var inspectBtn = document.getElementById(myJson.data[i].type);
+			    	for (i = 0; i < myJson.length; i++) { 
+			    		var inspectBtn = document.getElementById(myJson[i].type);
 						//console.log("inspectBnt: " + inspectBtn);
 						inspectBtn.addEventListener("click", function(){ goToFallaDetalles( this.getAttribute("id") )  });
 					}
@@ -135,6 +138,7 @@ function getData(){
 		setCookie("tipoFalla", tipoFalla, 1);
 		var myCookie = getCookie("tipoFalla");
 		console.log("Cookie guardada: " +  myCookie);
+		location.href = "/DetallesFalla";
 
 	//AQUI ABAJO VA EL CODIGO PARA PEDIRLE A LA API QUE ME MANDE A LA PAGINA DE FAILURE_DETAILS.HTML
 
@@ -146,11 +150,23 @@ function getData(){
 		cardContainer.html("");
 		
 		var newElement='';
-		for (i = rangoSuperior - 10; i < rangoSuperior && i < myJson.data.length; i++) { 
-			newElement += '<div class="row"><div class="col s10 m3"><div class="card"><div class="card-image"><img src="../images/placeholder.png"><span class="card-title" align="center">Falla ' + myJson.data[i].type + '</span></div><div class="card-content"><p>Cantidad de eventos: ' + myJson.data[i].events.length + "\n Eventos anormales: " + myJson.data[i].anevents + " \n Orden: " + (i+1) + '</p></div><div class="card-action"><a id="'+ myJson.data[i].type +'" href="/DetallesFalla">Inspeccionar</a></div></div></div>';
+		for (i = rangoSuperior - 10; i < rangoSuperior && i < myJson.length; i++) { 
+			//newElement += '<div class="row"><div class="col s10 m3"><div class="card"><div class="card-image"><img src="../images/placeholder.png"><span class="card-title" align="center">Falla ' + myJson.data[i].type + '</span></div><div class="card-content"><p>Cantidad de eventos: ' + myJson.data[i].events.length + "\n Eventos anormales: " + myJson.data[i].anevents + " \n Orden: " + (i+1) + '</p></div><div class="card-action"><a id="'+ myJson.data[i].type +'" href="/DetallesFalla">Inspeccionar</a></div></div></div>';
+		    newElement += '<div class="row"><div class="col s10 m3"><div class="card"><p class="card-title" align="center" >Falla ' + myJson[i].type + '</p><div class="card-content" style="padding-top:0px;">'+ '<p class="emphasize" "align="center" >' + myJson[i].anevents + '</p><p align="center"> Eventos anómalos</p><p align="center">Cantidad de eventos: ' + myJson[i].events.length  +  '</p></div><div class="card-action"><a id="'+ myJson[i].type +'" align="center" >Inspeccionar</a></div></div></div>';
+
 		}
 
+		//Ponle un listener a todos los boton 'inspect' de cada card
 		cardContainer.append(newElement);
+
+		for (i = 0; i < myJson.length; i++) { 
+	  		var inspectBtn = document.getElementById(myJson[i].type);
+			//console.log("inspectBnt: " + inspectBtn);
+			inspectBtn.addEventListener("click", function(){ goToFallaDetalles( this.getAttribute("id") )  });
+		}
+
+
+
 
 
 	}
@@ -160,12 +176,20 @@ function getData(){
 		cardContainer.html("");
 
 		var newElement='';
-		for (i = 0; i < myJson.data.length; i++) { 
+		for (i = 0; i < myJson.length; i++) { 
     		//cambié de span a div 
-    		newElement += '<div class="row"><div class="col s10 m3"><div class="card"><div class="card-image"><img src="../images/placeholder.png"><span class="card-title" align="center">Falla ' + myJson.data[i].type + '</span></div><div class="card-content"><p>Cantidad de eventos: ' + myJson.data[i].events.length + "\n Eventos anormales: " + myJson.data[i].anevents + " \n Orden: " + (i+1) + '</p></div><div class="card-action"><a id="'+ myJson.data[i].type +'" href="/DetallesFalla">Inspeccionar</a></div></div></div>';
+    		//newElement += '<div class="row"><div class="col s10 m3"><div class="card"><div class="card-image"><img src="../images/placeholder.png"><span class="card-title" align="center">Falla ' + myJson.data[i].type + '</span></div><div class="card-content"><p>Cantidad de eventos: ' + myJson.data[i].events.length + "\n Eventos anormales: " + myJson.data[i].anevents + " \n Orden: " + (i+1) + '</p></div><div class="card-action"><a id="'+ myJson.data[i].type +'" href="/DetallesFalla">Inspeccionar</a></div></div></div>';
+	   	    newElement += '<div class="row"><div class="col s10 m3"><div class="card"><p class="card-title" align="center" >Falla ' + myJson[i].type + '</p><div class="card-content" style="padding-top:0px;">'+ '<p class="emphasize" "align="center" >' + myJson[i].anevents + '</p><p align="center"> Eventos anómalos</p><p align="center">Cantidad de eventos: ' + myJson[i].events.length  +  '</p></div><div class="card-action"><a id="'+ myJson[i].type +'" align="center" >Inspeccionar</a></div></div></div>';
 	   	}
 
 	   	cardContainer.append(newElement);
+
+		//Ponle un listener a todos los boton 'inspect' de cada card
+	   	for (i = 0; i < myJson.length; i++) { 
+	  		var inspectBtn = document.getElementById(myJson[i].type);
+			//console.log("inspectBnt: " + inspectBtn);
+			inspectBtn.addEventListener("click", function(){ goToFallaDetalles( this.getAttribute("id") )  });
+		}
 
 
 	}
@@ -175,13 +199,22 @@ function getData(){
 		cardContainer.html("");
 
 		var newElement='';
-		for (i = 0; i < myJson.data.length; i++) { 
+		for (i = 0; i < myJson.length; i++) { 
     		//cambié de span a div 
-    		if (myJson.data[i].type == tipo)
-    			newElement += '<div class="row"><div class="col s10 m3"><div class="card"><div class="card-image"><img src="../images/placeholder.png"><span class="card-title" align="center">Falla ' + myJson.data[i].type + '</span></div><div class="card-content"><p>Cantidad de eventos: ' + myJson.data[i].events.length + "\n Eventos anormales: " + myJson.data[i].anevents + " \n Orden: " + (i+1) + '</p></div><div class="card-action"><a id="'+ myJson.data[i].type +'" href="/DetallesFalla">Inspeccionar</a></div></div></div>';
+    		if (myJson[i].type == tipo)
+    			//newElement += '<div class="row"><div class="col s10 m3"><div class="card"><div class="card-image"><img src="../images/placeholder.png"><span class="card-title" align="center">Falla ' + myJson.data[i].type + '</span></div><div class="card-content"><p>Cantidad de eventos: ' + myJson.data[i].events.length + "\n Eventos anormales: " + myJson.data[i].anevents + " \n Orden: " + (i+1) + '</p></div><div class="card-action"><a id="'+ myJson.data[i].type +'" href="/DetallesFalla">Inspeccionar</a></div></div></div>';
+	   			newElement += '<div class="row"><div class="col s10 m3"><div class="card"><p class="card-title" align="center" >Falla ' + myJson[i].type + '</p><div class="card-content" style="padding-top:0px;">'+ '<p class="emphasize" "align="center" >' + myJson[i].anevents + '</p><p align="center"> Eventos anómalos</p><p align="center">Cantidad de eventos: ' + myJson[i].events.length  +  '</p></div><div class="card-action"><a id="'+ myJson[i].type +'" align="center" >Inspeccionar</a></div></div></div>';
 	   	}
 
+
 	   	cardContainer.append(newElement);
+
+		//Ponle un listener a todos los boton 'inspect' de cada card
+	   	for (i = 0; i < myJson.length; i++) { 
+	  		var inspectBtn = document.getElementById(myJson[i].type);
+			//console.log("inspectBnt: " + inspectBtn);
+			inspectBtn.addEventListener("click", function(){ goToFallaDetalles( this.getAttribute("id") )  });
+		}
 
 
 	}
@@ -189,13 +222,13 @@ function getData(){
 
 	function orderFallas(){
 
-		for (i = 0; i < myJson.data.length; i++) { 
-			for (j = i+1; j < myJson.data.length; j++) { 
-				if (myJson.data[i].anevents < myJson.data[j].anevents){
+		for (i = 0; i < myJson.length; i++) { 
+			for (j = i+1; j < myJson.length; j++) { 
+				if (myJson[i].anevents < myJson[j].anevents){
 
-					var temp = myJson.data[i];
-					myJson.data[i] = myJson.data[j];
-					myJson.data[j] = temp;
+					var temp = myJson[i];
+					myJson[i] = myJson[j];
+					myJson[j] = temp;
 
 				}
 		   	}
