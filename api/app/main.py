44e9feeaf306c536daea980ff515a56f64f93f65
Victor Hugo Oyervides Covarrubias <victor.covarrubias@hotmail.com>
2020-02-07 03:28:13 -0600

    fix file upload

--- api/app/main.py
+++ api/app/main.py
@@ -48,28 +48,20 @@ def detail_route():
 #Route to upload the files to the server
 @app.route('/upload', methods=['POST'])
 def upload_file():
-    if request.method == 'POST':
-        # Check if we have the post data required to upload the file
-        if verifyPOSTData(request):
-            return jsonify( 
-                error=True, 
-                description=verifyPOSTData(request) 
-            )
-        file = request.files['file']
-        #Check the file
-        if verifyFile(file):
-            return jsonify( 
-                error=True, 
-                description=verifyFile(file)
-            )
-        #Conseguir el nombre y el directorio del archivo
-        filename = secure_filename(file.filename)
-        fileLocationAndName = os.path.join(app.config['UPLOAD_FOLDER'], filename)
-        file.save(fileLocationAndName)
-        #Call the script to parse the file data into a MongoDB document
-        response = parseFile(fileLocationAndName)
-        x = mycol.insert_one(response)
-        return jsonify(response)
+    #Read the data
+    response = parseFile()
+    x = mycol.delete_many({})
+    x = mycol.insert_many(response)
+    return jsonify(response)
+
+#Rute to populate the DB
+@app.route('/populate', methods=['GET'])
+def populate_data(): 
+    #Read the data
+    response = parseFile()
+    x = mycol.delete_many({})
+    x = mycol.insert_many(response)
+    return "Success"
     
 #Route to get all the data
 @app.route('/all-data', methods=['GET'])
