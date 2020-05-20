bcf6feacba5631b3afb6a1a07014c04b8986a99c
Francisco Simón <franciscosimon9600@gmail.com>
2020-05-20 08:36:11 -0500

    Deployment actualizado

--- proyecto/server.js
+++ proyecto/server.js
@@ -16,8 +16,20 @@ app.use("/dashboard", index);
 var ruta1 = require("./routes/fallas");
 app.use("/fallas", ruta1);
 
-var ruta2 = require("./routes/ruta2");
-app.use("/ruta2", ruta2);
+var ruta2 = require("./routes/tablelist");
+app.use("/tablelist", ruta2);
+
+var ruta3 = require("./routes/notifications");
+app.use("/notifications", ruta3);
+
+var ruta4 = require("./routes/users");
+app.use("/users", ruta4);
+
+var ruta5 = require("./routes/icons");
+app.use("/icons", ruta5);
+
+var ruta6 = require("./routes/typography");
+app.use("/typography", ruta6);
 
 app.listen(3000, function () {
   console.log("Server starting");
